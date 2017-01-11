4ed3cbed47bdbfd3b7722c4f3ab6ae34dd81ff8e
yaitloutou <yaitloutou@gmail.com>
2017-01-11 18:07:01 +0000

    :sparkles: :gem: last modified date plagin
    include last modified date qutomaticly into posts

---
+++ _plugins/hook-add-last-modified-date.rb
@@ -0,0 +1,11 @@
+# src: http://stackoverflow.com/a/36769049/2245329
+
+Jekyll::Hooks.register :posts, :pre_render do |post|
+
+  # get the current post last modified time
+  modification_time = File.mtime( post.path )
+
+  # inject modification_time in post's datas.
+  post.data['last-modified-date'] = modification_time
+
+end
